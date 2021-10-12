826bd1b9cb157fa4cdeff23a40848e147554f4d8
matthewhiggins415 <mhiggins.louis@gmail.com>
2021-10-11 22:09:09 -0500

    add all the ui/ux added. curl scripts next.

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 // user require with a reference to bundle the file and use it in this file
 // var example = require('./example');
 
-import 'bootstrap'
-
 // allows usage of new JS features
-require('babel-polyfill')
+require("babel-polyfill");
 
 // load manifests
 // javascript entry point
-require('./app/app.js')
+require("./app/app.js");
 
 // styles
-require('./app/styles/index.scss')
+require("./app/styles/styles.css");
